1659d7042de803a5a691ebf390bfb5edb5d3e12b
dolzhikov008@gmail.com <dalikona@gmail.com>
2024-03-10 15:00:07 +0300

    удаление графической задачи

---
+++ main.py
@@ -0,0 +1,17 @@
+def remove_tasks(tasks: list):
+    print_tasks(tasks)
+def ui();
+    tasks == remove_tasks()
+
+    if 
+
+
+    if  choice == input("какую убрать милорд"):
+        tasks.p = delete 
+        tasks = remove_tasks(tasks)
+
+
+
+
+
+
