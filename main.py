f330a8d0d97b8180e5ce9fd793cf8856613ad86d
Dima <you@example.com>
2024-03-17 14:54:43 +0300

    мне все равно придется менять на апенд

--- main.py
+++ main.py
@@ -3,16 +3,12 @@ def addtext(list):
     input("напиши какую")
     if list1:
         list1 = input("введите название задачи:")
-        print(list1)
     elif list2:
         list2 = input("введите название задачи:")
-        print(list2)
     elif list3:
         list3 = input("введите название задачи:")
-        print(list3)
     elif list4:
         list4 = input("введите название задачи:")
-        print(list4)
     elif list5:
         list5 = input("введите название задачи:")
-        print(list5)
+        return tasks
