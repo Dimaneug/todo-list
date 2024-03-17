b7d5b63d36b38546ef362ef44a9d2bcc7c203d62
Dima <you@example.com>
2024-03-17 14:29:34 +0300

    написал больше кода

---
+++ main.py
@@ -0,0 +1,12 @@
+def addtext(list):
+    print("какую задачу хотите создать:list1,list2,list3")
+    input()
+    if list1:
+        list1 = input("введите название задачи:")
+        print(list1)
+    elif list2:
+        list2 = input("введите название задачи:")
+        print(list2)
+    elif list3:
+        list3 = input("введите название задачи:")
+        print(list3)
