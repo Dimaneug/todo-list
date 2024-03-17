8900f2ab1641427bd7064ecfeb7f48ebe7443b6e
dolzhikov008@gmail.com <dalikona@gmail.com>
2024-03-17 14:54:37 +0300

    юху мистер бисттт

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 def remove_tasks(tasks: list):
     print_tasks(tasks)
-def ui();
-    tasks == remove_tasks()
-
-    if 
+    list=[]
+    
 
 
-    if  choice == input("какую убрать милорд"):
-        tasks.p = delete 
-        tasks = remove_tasks(tasks)
+    choice = input("какую убрать милорд")
+    n=int(choice)
+    tasks.pop(choice)
+    list_save = list
+    pot=input("потвердите удаление задачи,1-удалить,2-отмена")
+    print (pot)
+    a=int(input())
+    if a == 1:
+        tasks.pop(input())
+    else:
+        list == list_save 
+
+   
+def print_tasks():
+    pass
+
+remove_tasks(["1","2","3"])
+
+
+
 
 
 
