2bca4147c784fd716e46405d5f85c52f47f6d18b
1337 <1337konuhov7331@gmail.com>
2024-03-31 13:49:38 +0300

    popaaaa

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 def read_tasks():
     tasks = []
-    with open("file", "r") as file:
-        file.read
+    with open("file", "r", encoding="utf-8") as file:
+        tasks = file.readlines()
+    for i in range(len(tasks)):
+        tasks[i] = tasks[i][:-1]
+
     return tasks
 
 print(read_tasks())
