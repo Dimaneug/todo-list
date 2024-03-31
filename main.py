f7c93dae6b0392674a57467d888edf69d907b273
Dima <you@example.com>
2024-03-31 14:19:36 +0300

    Скорее всего это финал

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
+list = []
+ded = input("Введите название задачи")
 def addtext(list):
-    print("какую задачу хотите создать:list1,list2,list3,list4,list5 ?")
-    input("напиши какую")
-    if list1:
-        list1 = input("введите название задачи:")
-    elif list2:
-        list2 = input("введите название задачи:")
-    elif list3:
-        list3 = input("введите название задачи:")
-    elif list4:
-        list4 = input("введите название задачи:")
-    elif list5:
-        list5 = input("введите название задачи:")
-        return tasks
+    list.append(ded)
